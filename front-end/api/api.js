6a9a3692b38899b4a36599d7e804febfa7b17b7e
Araujo <araujo280465@gmail.com>
2025-08-01 13:34:06 -0400

    ajuste no process env

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -2,9 +2,11 @@
 import axios from "axios";
 import "dotenv/config";
 
-const { NODE_ENV } = process.env;
-// const URL = "http://localhost:3001/api";
-const URL = NODE_ENV === "development" ? "http://localhost:3001/api" : "/api";
+//const { NODE_ENV } = process.env;
+//const URL = "http://localhost:3001/api";
+const URL = "https://deploy-jornada-full-stack-tyxi.onrender.com/api";
+
+//const URL = NODE_ENV === "development" ? "http://localhost:3001/api" : "/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
